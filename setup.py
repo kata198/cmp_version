32720fd10dcbd8dc0cfcf681bac20e9402ed0b7e
Tim Savannah <kata198@gmail.com>
2016-10-25 02:12:26 -0400

    2.1.0 (not yet released)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='cmp_version',
-            version='2.0.0',
+            version='2.1.0',
             packages=['cmp_version'],
             scripts=['cmp-version'],
             author='Tim Savannah',
