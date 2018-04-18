3b98e1dde9fe0bc5948c02d8efeac362797f072b
Tim Savannah <kata198@gmail.com>
2018-04-17 20:36:34 -0400

    Add Epoch support with versions

--- cmp_version/__init__.py
+++ cmp_version/__init__.py
@@ -26,13 +26,15 @@ __version_tuple__ = (2, 1, 1)
 
 ALPHA_OR_NUM_RE = re.compile('([a-zA-Z]+)|([0-9]+)')
 
+EPOCH_RE = re.compile('^(?P<epoch>[0-9]+):(?P<version>.+)$')
 
-# TODO: Handle epoch versions as well (like 5:1.2.3 > 4:3.2.1)?
 def cmp_version(version1, version2):
     '''
         cmp_version - Compare two strings which contain versions, checking which one represents a "newer" (greater) release.
 
         Note that even if two version strings are not equal string-wise, they may still be equal version-wise (e.x. 1.0.0 is the same version as 1.0)
+
+        This supports handling "release" parts (-whatever) and "Epochs" ( N:version )
 
         @param version1 <str> - A string containing a version
         @param version2 <str> - A string containing a version
@@ -46,6 +48,22 @@ def cmp_version(version1, version2):
 
             cmp_version('1.0.5b', '1.0.5a') would return 1 because 1.0.5b comes after 1.0.5a
     '''
+    # Call the "internal" function so we can call recursively and apply logic like
+    #   "only if at the start of version" (consideration for Epoch) accurately
+    #   without fudging up the public interface
+    return _cmp_version_internal(version1, version2)
+
+
+def _cmp_version_internal(version1, version2, versionIdxNum=0):
+    '''
+        _cmp_version_internal - Internal worker for cmp_version
+            
+            @param versionIdxNum - The version "part" (e.x. split on release)
+
+
+            @see cmp_version
+
+    '''
     # Ensure we are using strings, so VersionString doesn't recurse
     version1 = str(version1) or '0'
     version2 = str(version2) or '0'
@@ -55,6 +73,36 @@ def cmp_version(version1, version2):
     if version1 == version2:
         return 0
 
+    
+    if versionIdxNum == 0:
+        # If we are on the first part of version, 
+        #  check if we have epochs to consider
+        #    Default to epoch 0
+        epochMatch1 = EPOCH_RE.match(version1)
+        if epochMatch1:
+            epochMatch1Groupdict = epochMatch1.groupdict()
+
+            epoch1 = int(epochMatch1Groupdict['epoch'] or '0')
+            version1 = epochMatch1Groupdict['version'] or '0'
+        else:
+            epoch1 = 0
+
+        epochMatch2 = EPOCH_RE.match(version2)
+        if epochMatch2:
+            epochMatch2Groupdict = epochMatch2.groupdict()
+
+            epoch2 = int(epochMatch2Groupdict['epoch'] or '0')
+            version2 = epochMatch2Groupdict['version'] or '0'
+        else:
+            epoch2 = 0
+
+        # If epochs are not equal, the higher one wins no matter what
+        if epoch1 > epoch2:
+            return 1
+        elif epoch1 < epoch2:
+            return -1
+    
+
     # If there is a release portion, split it out and treat it as a lower-proirity version compare
     version1ReleaseSplit = version1.split('-') #re.split('-', version1)
     version2ReleaseSplit = version2.split('-') #re.split('-', version2)
@@ -62,7 +110,7 @@ def cmp_version(version1, version2):
     # If we have at least one release
     if len(version1ReleaseSplit) > 1 or len(version2ReleaseSplit) > 1:
         # Compare the first part as normal version
-        firstPartCmp = cmp_version(version1ReleaseSplit[0], version2ReleaseSplit[0])
+        firstPartCmp = _cmp_version_internal(version1ReleaseSplit[0], version2ReleaseSplit[0], versionIdxNum+1)
         if firstPartCmp != 0:
             # If first part is not equal, return that
             return firstPartCmp
@@ -82,7 +130,7 @@ def cmp_version(version1, version2):
                 nextVal2 = '0'
             
             # Compare second entry
-            cmpRes = cmp_version(nextVal1, nextVal2)
+            cmpRes = _cmp_version_internal(nextVal1, nextVal2, versionIdxNum+1)
             if cmpRes != 0:
                 # If not equal, return result
                 return cmpRes
@@ -92,7 +140,7 @@ def cmp_version(version1, version2):
                     return 0
                 else:
                     # Recurse on remaining parts
-                    return cmp_version('-'.join(version1ReleaseSplit), '-'.join(version2ReleaseSplit))
+                    return _cmp_version_internal('-'.join(version1ReleaseSplit), '-'.join(version2ReleaseSplit), versionIdxNum+1)
 
 
     # If prefixed or suffixed with a ., insert a '0'
